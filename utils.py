da96038ff604a8c3d82b862e790ffc88f68bad87
JichengLi98 <jichengli@ln.hk>
2025-10-20 14:31:36 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,21 @@
+def split_sequences_TransMIT(sequences, s):
+    X, y = list(),list()
+    for i in range(len(sequences)):
+        end_ix = i + s + 1 
+        # check if we are beyond the dataset
+        if end_ix > len(sequences):
+            break
+        seq_x, seq_y = sequences[i:end_ix, :], sequences[end_ix-1, :] 
+        X.append(seq_x)
+        y.append(seq_y)
+    return np.array(X), np.array(y)
+
+
+
+
+
+
+
+
+
 
