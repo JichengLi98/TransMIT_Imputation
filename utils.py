1a6100e1da91669f548e83d1840a028e55aa886b
JichengLi98 <jichengli@ln.hk>
2025-10-21 17:48:17 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,17 @@ def split_sequences_TransMIT(sequences, s):
     return np.array(X), np.array(y)
 
 
-
+def train_test_split(data, missing_matrix, train_size,s):
+  train = data[:train_size,:]
+  test = data[train_size-s:,:]
+  missing_train = missing_matrix[:train_size,:]
+  missing_test = missing_matrix[train_size-n_steps:,:]
+  train_mask = train*missing_train
+  test_mask = test*missing_test
+  #sliding window
+  train_x, train_y = split_sequences_TransMIT(train, n_steps)
+  train_x[:,-1,:] = train_mask[n_steps:,:] 
+return train_x, train_y, test, test_mask
 
 
 
