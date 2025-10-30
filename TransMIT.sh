b43c34dbb6ef88a0a70a5cc1e37339360ac4cf5c
JichengLi98 <jichengli@ln.hk>
2025-10-30 17:15:32 +0800

    Update TransMIT.sh

--- TransMIT.sh
+++ TransMIT.sh
@@ -13,54 +13,54 @@ python -u run.py \
   --num_layers 4 \
   --num_heads 4
 
-# python -u run.py \
-#   --data_name 'FTD' \
-#   --miss_rate 0.2 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+python -u run.py \
+  --data_name 'FTD' \
+  --miss_rate 0.2 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
 
-#   python -u run.py \
-#   --data_name 'FTD' \
-#   --miss_rate 0.3 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+  python -u run.py \
+  --data_name 'FTD' \
+  --miss_rate 0.3 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
 
-#   python -u run.py \
-#   --data_name 'FTD' \
-#   --miss_rate 0.4 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+  python -u run.py \
+  --data_name 'FTD' \
+  --miss_rate 0.4 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
 
-#   python -u run.py \
-#   --data_name 'FTD' \
-#   --miss_rate 0.5 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+  python -u run.py \
+  --data_name 'FTD' \
+  --miss_rate 0.5 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
