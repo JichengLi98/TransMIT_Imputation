143c616f62dbedd1ca482d65b2bffd038c860302
JichengLi98 <jichengli@ln.hk>
2025-10-31 22:41:35 +0800

    Update TransMIT.sh

--- TransMIT.sh
+++ TransMIT.sh
@@ -1,56 +1,56 @@
 export CUDA_VISIBLE_DEVICES=0
 
-python -u run.py \
-  --data_name 'FTD' \
-  --miss_rate 0.1 \
-  --batch_size 100\
-  --lr 0.0005 \
-  --epochs 200 \
-  --alpha 0.5 \
-  --s 16 \
-  --d_model 128 \
-  --d_q 64 \
-  --num_layers 4 \
-  --num_heads 4
+# python -u run.py \
+#   --data_name 'FTD' \
+#   --miss_rate 0.1 \
+#   --batch_size 100\
+#   --lr 0.0005 \
+#   --epochs 200 \
+#   --alpha 0.5 \
+#   --s 16 \
+#   --d_model 128 \
+#   --d_q 64 \
+#   --num_layers 4 \
+#   --num_heads 4
 
-python -u run.py \
-  --data_name 'FTD' \
-  --miss_rate 0.2 \
-  --batch_size 100\
-  --lr 0.0005 \
-  --epochs 200 \
-  --alpha 0.5 \
-  --s 16 \
-  --d_model 128 \
-  --d_q 64 \
-  --num_layers 4 \
-  --num_heads 4
+# python -u run.py \
+#   --data_name 'FTD' \
+#   --miss_rate 0.2 \
+#   --batch_size 100\
+#   --lr 0.0005 \
+#   --epochs 200 \
+#   --alpha 0.5 \
+#   --s 16 \
+#   --d_model 128 \
+#   --d_q 64 \
+#   --num_layers 4 \
+#   --num_heads 4
 
-  python -u run.py \
-  --data_name 'FTD' \
-  --miss_rate 0.3 \
-  --batch_size 100\
-  --lr 0.0005 \
-  --epochs 200 \
-  --alpha 0.5 \
-  --s 16 \
-  --d_model 128 \
-  --d_q 64 \
-  --num_layers 4 \
-  --num_heads 4
+#   python -u run.py \
+#   --data_name 'FTD' \
+#   --miss_rate 0.3 \
+#   --batch_size 100\
+#   --lr 0.0005 \
+#   --epochs 200 \
+#   --alpha 0.5 \
+#   --s 16 \
+#   --d_model 128 \
+#   --d_q 64 \
+#   --num_layers 4 \
+#   --num_heads 4
 
-  python -u run.py \
-  --data_name 'FTD' \
-  --miss_rate 0.4 \
-  --batch_size 100\
-  --lr 0.0005 \
-  --epochs 200 \
-  --alpha 0.5 \
-  --s 16 \
-  --d_model 128 \
-  --d_q 64 \
-  --num_layers 4 \
-  --num_heads 4
+#   python -u run.py \
+#   --data_name 'FTD' \
+#   --miss_rate 0.4 \
+#   --batch_size 100\
+#   --lr 0.0005 \
+#   --epochs 200 \
+#   --alpha 0.5 \
+#   --s 16 \
+#   --d_model 128 \
+#   --d_q 64 \
+#   --num_layers 4 \
+#   --num_heads 4
 
   python -u run.py \
   --data_name 'FTD' \
