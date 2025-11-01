9c98cf2aa2108167e69f9fdecf248ef99de6abe2
JichengLi98 <jichengli@ln.hk>
2025-11-01 23:53:22 +0800

    Update TransMIT3.sh

--- TransMIT3.sh
+++ TransMIT3.sh
@@ -16,19 +16,19 @@ fix_seed=${1:-2025}  # default = 2025 if no argument provided
 #   --num_layers 4 \
 #   --num_heads 4
 
-python -u run.py \
-  --fix_seed $fix_seed \
-  --data_name 'FTD' \
-  --miss_rate 0.2 \
-  --batch_size 100\
-  --lr 0.0005 \
-  --epochs 200 \
-  --alpha 0.5 \
-  --s 16 \
-  --d_model 128 \
-  --d_q 64 \
-  --num_layers 4 \
-  --num_heads 4
+# python -u run.py \
+#   --fix_seed $fix_seed \
+#   --data_name 'FTD' \
+#   --miss_rate 0.2 \
+#   --batch_size 100\
+#   --lr 0.0005 \
+#   --epochs 200 \
+#   --alpha 0.5 \
+#   --s 16 \
+#   --d_model 128 \
+#   --d_q 64 \
+#   --num_layers 4 \
+#   --num_heads 4
 
 #   python -u run.py \
 #   --fix_seed $fix_seed \
@@ -58,16 +58,16 @@ python -u run.py \
 #   --num_layers 4 \
 #   --num_heads 4
 
-# python -u run.py \
-#   --fix_seed $fix_seed \
-#   --data_name 'FTD' \
-#   --miss_rate 0.5 \
-#   --batch_size 100\
-#   --lr 0.0005 \
-#   --epochs 200 \
-#   --alpha 0.5 \
-#   --s 16 \
-#   --d_model 128 \
-#   --d_q 64 \
-#   --num_layers 4 \
-#   --num_heads 4
+python -u run.py \
+  --fix_seed $fix_seed \
+  --data_name 'FTD' \
+  --miss_rate 0.5 \
+  --batch_size 100\
+  --lr 0.0005 \
+  --epochs 200 \
+  --alpha 0.5 \
+  --s 16 \
+  --d_model 128 \
+  --d_q 64 \
+  --num_layers 4 \
+  --num_heads 4
